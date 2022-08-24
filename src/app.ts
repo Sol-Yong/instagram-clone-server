74f7f3e842110611196c2e2e3e0c33b6f595f386
Yongveloper <aaa9015@naver.com>
2022-08-24 16:25:53 +0900

    [구성] 프로젝트 초기 환경 셋팅 typescript, express, cors,helmet, morgan

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(404);
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+
+app.listen(8080, () => {
+  console.log('Started!');
+});
